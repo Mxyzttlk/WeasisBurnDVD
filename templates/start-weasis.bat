b3f50355cf04fa76331822952d9c60da3552ac17
Mxyzttlk <gheorghiirotaru13@gmail.com>
2026-02-21 11:48:16 +0200

    768 MB → 1280 MB

--- templates/start-weasis.bat
+++ templates/start-weasis.bat
@@ -11,4 +11,4 @@ REM Clean old Weasis cache to avoid slow startup from corrupted bundles
 for /d %%D in ("%USERPROFILE%\.weasis\cache-*") do rmdir /s /q "%%D" 2>nul
 
 REM Launch Weasis using full paths (required for optical media)
-start "Weasis" "%~dp0jre\windows\bin\javaw.exe" -Xms64m -Xmx768m -Dweasis.portable.dir="%~dp0." -Dgosh.args="-sc telnetd -p 17179 start" -cp "%~dp0weasis-launcher.jar;%~dp0felix.jar;%~dp0substance.jar" org.weasis.launcher.WeasisLauncher $dicom:get --portable
+start "Weasis" "%~dp0jre\windows\bin\javaw.exe" -Xms64m -Xmx1280m -Dweasis.portable.dir="%~dp0." -Dgosh.args="-sc telnetd -p 17179 start" -cp "%~dp0weasis-launcher.jar;%~dp0felix.jar;%~dp0substance.jar" org.weasis.launcher.WeasisLauncher $dicom:get --portable
